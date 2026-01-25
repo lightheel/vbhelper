c990a495b77035ec486af782561974df3123ffe1
nacabaro <nacabaro@nacatech.es>
2026-01-25 19:58:03 +0100

    Fix Special Missions a bit more Now they should behave a bit better, and it will not allow the watch to have multiple missions of the same type, plus you can now delete special missions.

--- app/src/main/java/com/github/nacabaro/vbhelper/screens/homeScreens/screens/VBDiMHomeScreen.kt
+++ app/src/main/java/com/github/nacabaro/vbhelper/screens/homeScreens/screens/VBDiMHomeScreen.kt
@@ -10,6 +10,10 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,6 +30,7 @@ import com.github.nacabaro.vbhelper.screens.homeScreens.HomeScreenControllerImpl
 import com.github.nacabaro.vbhelper.utils.BitmapData
 import java.util.Locale
 import androidx.compose.ui.res.stringResource
+import com.github.nacabaro.vbhelper.screens.homeScreens.dialogs.DeleteSpecialMissionDialog
 
 
 @Composable
@@ -39,6 +44,8 @@ fun VBDiMHomeScreen(
     contentPadding: PaddingValues,
     onClickCollect: (ItemDtos.PurchasedItem?, Int?) -> Unit
 ) {
+    var selectedSpecialMissionId by remember { mutableStateOf<Long>(-1) }
+
     Column(
         modifier = Modifier
             .padding(top = contentPadding.calculateTopPadding())
@@ -183,11 +190,28 @@ fun VBDiMHomeScreen(
                     modifier = Modifier
                         .weight(1f)
                         .padding(8.dp),
-                ) {
-                    homeScreenController
-                        .clearSpecialMission(mission.id, mission.status, onClickCollect)
-                }
+                    onClickMission = { missionId ->
+                        selectedSpecialMissionId = missionId
+                    },
+                    onClickCollect = {
+                        homeScreenController
+                            .clearSpecialMission(selectedSpecialMissionId, onClickCollect)
+                    }
+                )
             }
         }
+    }
+
+    if (selectedSpecialMissionId.toInt() != -1) {
+        DeleteSpecialMissionDialog(
+            onClickDismiss = {
+                selectedSpecialMissionId = -1
+            },
+            onClickDelete = {
+                homeScreenController
+                    .clearSpecialMission(selectedSpecialMissionId, onClickCollect)
+                selectedSpecialMissionId = -1
+            }
+        )
     }
 }
